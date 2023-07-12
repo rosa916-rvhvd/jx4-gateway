bee1f1bc17f56800f8c0b92ad038311ff03dbfc9
Josh Wood <j@joshix.com>
2023-07-12 20:19:27 +0000

    Add endpoint

--- src/main/java/com/rhdevelopers/summit/workshop/SummitBackendResource.java
+++ src/main/java/com/rhdevelopers/summit/workshop/SummitBackendResource.java
@@ -70,7 +70,6 @@ public class SummitBackendResource {
 
     //Uncomment the Java method below to add an API endpoint to add your own data points.
     
-    /*
     @POST
     @Path("poi/add/point")
     public Response loadCustomPoi(PoiRecord record) {
@@ -79,7 +78,6 @@ public class SummitBackendResource {
             UriBuilder.fromPath("fake/poi/find/"+(SUMMIT_POI_RECORDS.size()-1)).build()
         ).entity("inserted custom data point").build();
     }
-    */
 
     //How to use it:
     //The coordinates number array in the JSON snippet is specified [lat,lng] -> so latitude first, then longitude.
